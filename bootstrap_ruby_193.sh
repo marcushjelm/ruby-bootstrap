6abe9b3666bb9dfa1e85739cb476cb9d9f4ad774
Marcus Hjelm <marcus@makemedia.se>
2013-02-06 15:01:58 +0100

    Update bootstrap_ruby_193.sh

--- bootstrap_ruby_193.sh
+++ bootstrap_ruby_193.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 apt-get -y update
-apt-get -y -m install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxslt-dev libxml2-dev libcurl4-openssl-dev libffi-dev libgdbm-dev ncurses-dev
+apt-get -y -m install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxslt1-dev libxml2-dev libcurl4-openssl-dev libffi-dev libgdbm-dev libncurses5-dev
 cd /tmp
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p327.tar.gz
-tar xzvf ruby-1.9.3-p327.tar.gz
-cd ruby-1.9.3-p327/
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p374.tar.gz
+tar xzvf ruby-1.9.3-p374.tar.gz
+cd ruby-1.9.3-p374/
 ./configure --prefix=/usr/local
 make
 make install
 gem update --system
-gem install bundler --no-ri --no-rdoc
+gem install bundler  --no-ri --no-rdoc
 ruby -v
