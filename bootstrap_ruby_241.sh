5859719d2728056f81901da0ebfe54137916fd57
Marcus Ahlford <marcus@makemedia.se>
2017-05-05 16:48:56 +0200

    Update bootstrap_ruby_241.sh

--- bootstrap_ruby_241.sh
+++ bootstrap_ruby_241.sh
@@ -2,7 +2,7 @@
 apt-get -y update
 apt-get -y -m install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxslt1-dev libxml2-dev libcurl4-openssl-dev libffi-dev libgdbm-dev libncurses5-dev
 cd /tmp
-wget http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.1.1.tar.gz
+wget http://cache.ruby-lang.org/pub/ruby/2.4/ruby-2.4.1.tar.gz
 tar xzvf ruby-2.4.1.tar.gz
 cd ruby-2.4.1/
 ./configure --prefix=/usr/local
